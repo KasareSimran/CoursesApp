f229d198447c861cf73701d91e6593df102a6160
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-17 23:21:27 +0530

    updated

--- src/main/java/com/springRest/courseApp/CourseAppApplication.java
+++ src/main/java/com/springRest/courseApp/CourseAppApplication.java
@@ -2,8 +2,10 @@ package com.springRest.courseApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication
 public class CourseAppApplication {
 
 	public static void main(String[] args) {
