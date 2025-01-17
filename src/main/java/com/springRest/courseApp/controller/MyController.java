c2a9b333c39526b9ece352f7af6b89669a153717
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-17 23:21:59 +0530

    updated

--- src/main/java/com/springRest/courseApp/controller/MyController.java
+++ src/main/java/com/springRest/courseApp/controller/MyController.java
@@ -6,6 +6,7 @@ import com.springRest.courseApp.model.Course;
 import com.springRest.courseApp.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,5 +27,11 @@ public class MyController   {
     @GetMapping("/courses")
     public List<Course> getCourses(){
         return this.courseService.getCourses();
+    }
+
+
+    @GetMapping("/courses/{courseId}")
+    public Course getCourse(@PathVariable String courseId){
+        return this.courseService.getCourse(Long.parseLong(courseId));
     }
 }
