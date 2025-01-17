6b01a84ae2d2e0021ab6094544f25281ebfac69c
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-17 22:39:37 +0530

    data added

--- src/main/java/com/springRest/courseApp/service/CourseServiceImpl.java
+++ src/main/java/com/springRest/courseApp/service/CourseServiceImpl.java
@@ -1,2 +1,22 @@
-package com.springRest.courseApp.service;public class CourseServiceImpl {
+package com.springRest.courseApp.service;
+
+import com.springRest.courseApp.model.Course;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service //spring framework will identityfy this is service and provide implementation which is going on service layer
+public class CourseServiceImpl implements CourseService{
+
+    List<Course> list;
+    public CourseServiceImpl(){
+            list=new ArrayList<>();
+            list.add(new Course(123,"java course","you will learn basics java"));
+            list.add(new Course(124,"spring course","you will learn basics of spring"));
+    }
+    @Override
+    public List<Course> getCourses() {
+        return list;
+    }
 }
