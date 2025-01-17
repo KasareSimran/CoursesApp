7994dfe4d973c0fbd547b6ec5497813410b00bc6
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-18 00:59:03 +0530

    updated logic

--- src/main/java/com/springRest/courseApp/service/CourseService.java
+++ src/main/java/com/springRest/courseApp/service/CourseService.java
@@ -10,5 +10,9 @@ public interface CourseService {
 
     public Course getCourse(long courseId);
 
-    Course addCourse(Course course);
+    public Course addCourse(Course course);
+
+    public Course updateCourse(Course course);
+
+    public boolean deleteCourse(long courseId);
 }
