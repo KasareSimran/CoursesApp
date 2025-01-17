dcec970f2ba5f236b281b8a69823c6a20ce72f10
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-17 22:38:24 +0530

    third commit

--- src/main/java/com/springRest/courseApp/controller/MyController.java
+++ src/main/java/com/springRest/courseApp/controller/MyController.java
@@ -3,6 +3,8 @@ package com.springRest.courseApp.controller;
 
 //import org.springframework.stereotype.Controller;
 import com.springRest.courseApp.model.Course;
+import com.springRest.courseApp.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,9 @@ import java.util.List;
 
 @RestController  //by adding this anotation now springboot will understand ki it have cass mycontroller
 public class MyController   {
+
+    @Autowired //springbbot will handle evrything ,will create obj.
+    private CourseService courseService;
 
     @GetMapping("/home")
     public String home(){
@@ -20,7 +25,6 @@ public class MyController   {
     //getting all courses
     @GetMapping("/courses")
     public List<Course> getCourses(){
-
-        return;
+        return this.courseService.getCourses();
     }
 }
