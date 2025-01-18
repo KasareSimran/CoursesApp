066ad0126b67f1ba36d56c4dcbe6f8b889b378ca
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-18 19:57:29 +0530

    updated

--- src/main/java/com/springRest/courseApp/CourseAppApplication.java
+++ src/main/java/com/springRest/courseApp/CourseAppApplication.java
@@ -2,10 +2,8 @@ package com.springRest.courseApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-//@SpringBootApplication
+@SpringBootApplication
 public class CourseAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springRest/courseApp/service/CourseServiceImpl.java
+++ src/main/java/com/springRest/courseApp/service/CourseServiceImpl.java
@@ -1,11 +1,18 @@
 package com.springRest.courseApp.service;
 
+import com.springRest.courseApp.model.Course;
 import com.springRest.courseApp.repository.courseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service //spring framework will identityfy this is service and provide implementation which is going on service layer
 public class CourseServiceImpl implements CourseService{
+
+
+//without jpa
+
 
 //    List<Course> list;
 //    public CourseServiceImpl(){
@@ -60,8 +67,45 @@ public class CourseServiceImpl implements CourseService{
 //    }
 
 
+
+  //with jpa
+
     @Autowired
     private courseRepo repo;
+
+    public CourseServiceImpl(){
+
+    }
+
+
+    @Override
+    public List<Course> getCourses() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Course getCourse(long courseId) {
+        return repo.getReferenceById(courseId);
+    }
+
+    @Override
+    public Course addCourse(Course course) {
+        repo.save(course);
+        return course;
+    }
+
+    @Override
+    public Course updateCourse(Course course) {
+        repo.save(course);
+        return course;
+    }
+
+    @Override
+    public boolean deleteCourse(long courseId) {
+        Course entity=repo.getReferenceById(courseId);
+        repo.delete(entity);
+        return false;
+    }
 
 
 }
