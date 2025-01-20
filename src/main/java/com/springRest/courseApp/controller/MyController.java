929696649446d0416e88cd9ca3b65063fc3014e4
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-21 00:29:51 +0530

    updated

--- src/main/java/com/springRest/courseApp/controller/MyController.java
+++ src/main/java/com/springRest/courseApp/controller/MyController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController  //by adding this anotation now springboot will understand ki it have cass mycontroller
 public class MyController   {
 
